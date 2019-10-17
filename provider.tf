6e8fc014bc4f75734138a401e71dffd402d6f859
Achmad Taufiq <43929042+atoamaeto@users.noreply.github.com>
2019-10-18 00:04:05 +0700

    Add providers
    Add providers

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "openstack" {
+  domain_name = "default"
+  user_name   = "YOUR-USERNAME"
+  tenant_name = "YOUR-PROJECT"
+  password    = "YOUR-PASSWORD"
+  auth_url    = "YOUR-KEYSTONE-API-URL"
+}
+
